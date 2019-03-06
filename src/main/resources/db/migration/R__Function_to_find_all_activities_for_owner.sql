c9ea7bc0cec04f8e6da0e64bd7f361a0d62a2a18
unrealview <michel.hirise@gmx.fr>
2019-03-06 15:27:47 +0100

    Fix 3.2 function to find all activities for a given owner

--- src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
+++ src/main/resources/db/migration/R__Function_to_find_all_activities_for_owner.sql
@@ -1,3 +1,7 @@
-CREATE OR REPLACE FUNCTION find_all_activities_for_owner(username varchar(40)) RETURNS  AS $$
- RETURNING ;
+CREATE OR REPLACE FUNCTION find_all_activities_for_owner(username varchar(40)) RETURNS SETOF activity AS $$
+SELECT A.*
+FROM activity A
+JOIN "user" U
+ON U.id = owner_id
+WHERE U.username = find_all_activities_for_owner.username
 $$ LANGUAGE SQL;
