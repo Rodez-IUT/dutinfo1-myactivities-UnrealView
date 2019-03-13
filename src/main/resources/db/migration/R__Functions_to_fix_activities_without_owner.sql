b452d359925cd663f9623e1a3fba102bcc561ba8
unrealview <michel.hirise@gmx.fr>
2019-03-13 16:13:30 +0100

    Fix 4.2 function to get default owner

---
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
+	DECLARE
+		defaultOwner "user"%rowtype;
+		defaultOwnerUsername varchar(500) := 'Default Owner';
+	BEGIN
+		SELECT * INTO defaultOwner from "user"
+		WHERE username = defaultOwnerUsername;
+		IF NOT FOUND THEN
+			INSERT INTO "user"(id, username)
+			VALUES (nextval('id_generator'), defaultOwnerUsername);
+			SELECT * INTO defaultOwner from "user"
+			WHERE username = defaultOwnerUsername;
+		END IF;
+		RETURN defaultOwner;
+	END 
+$$ LANGUAGE PLPGSQL;
