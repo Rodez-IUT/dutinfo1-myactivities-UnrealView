222781035d240facd1fcea208022db9e835f7a02
unrealview <michel.hirise@gmx.fr>
2019-03-27 16:13:50 +0100

    Fix 5.2 trigger to log action on activities

---
+++ src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
@@ -0,0 +1,12 @@
+DROP TRIGGER IF EXISTS log_delete_activity ON activity;
+CREATE OR REPLACE FUNCTION log_delete_activity() RETURNS TRIGGER AS $$
+BEGIN
+	INSERT INTO action_log 
+	VALUES (nextval('id_generator'),'delete','activity',OLD.id,user,now());
+	RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER log_delete_activity
+	AFTER DELETE ON activity
+	FOR EACH ROW EXECUTE PROCEDURE log_delete_activity();
